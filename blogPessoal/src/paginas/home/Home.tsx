76ae9f045fddb315981230231d9d2cd7ca583465
Alabakiz <dariowsp23@gmail.com>
2023-05-12 15:20:09 -0300

    Front parte1

---
+++ blogPessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+        </>
+    );
+}
+
+export default Home;
+
+
+/*
+<h1 
+            className ="titulo">Home</h1>
+            <img 
+            src="./src/assets/img/ImagemTop.jpg" 
+            alt="Pessoa abaixado com uma espada apoiada no ombro" 
+            className="img"/>
+
+            display="flex" justifyContent="center"
+*/
